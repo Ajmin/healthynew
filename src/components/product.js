16cb9839a575c12cb7b2a0527fc4e5cc1a977f85
suku <adarshpsukumaran@gmail.com>
2024-04-30 11:58:06 +0530

    recipe

--- src/components/product.js
+++ src/components/product.js
@@ -1,21 +1,25 @@
 import React from "react";
 import Cards from "./productcard.js";
 import { useLocation } from "react-router-dom";
+import backgroundImage from "/home/adarsh/new/src/images/recipe_bg.png"; // Import your background image
+
 
 export default function Product() {
   const { state } = useLocation();
   console.log(state.parsed[0].food.image);
   return (
-    <div className="product">
-      <div className="product-image">
-        <img src={state.parsed[0].food.image}></img>
+    <div className="background-wrapper" style={{ backgroundImage: `url(${backgroundImage})` }}>
+      <div className="product">
+        <div className="product-image">
+          <img src={state.parsed[0].food.image} alt="Product" />
+        </div>
+        <Cards
+          name={state.parsed[0].food.label}
+          calory={state.parsed[0].food.nutrients.ENERC_KCAL}
+          protein={state.parsed[0].food.nutrients.PROCNT}
+          fat={state.parsed[0].food.nutrients.FAT}
+        />
       </div>
-      <Cards
-        name={state.parsed[0].food.label}
-        calory={state.parsed[0].food.nutrients.ENERC_KCAL}
-        protein={state.parsed[0].food.nutrients.PROCNT}
-        fat={state.parsed[0].food.nutrients.FAT}
-      />
     </div>
   );
 }
--- src/components/productcard.js
+++ src/components/productcard.js
@@ -1,27 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function cards(props) {
+
+export default function Cards(props) {
+  const [isNutrientsVisible, setIsNutrientsVisible] = useState(true);
+
+  const toggleNutrientsVisibility = () => {
+    setIsNutrientsVisible(!isNutrientsVisible);
+  };
+
   return (
     <div>
       <div className="product-card">
         <div className="product-header">
-          <h3>{props.name}</h3>
-          <p1>description</p1>
-        </div>
-        <div className="nutrients">
-          <div className="nutrientssub">
-            <img src="protein" />
-            <p>{props.calory}</p>
-          </div>
-          <div className="nutrientssub">
-            <img src="protein" />
-            <p>{props.protein}</p>
-          </div>
-          <div className="nutrientssub">
-            <img src="protein" />
-            <p>{props.fat}</p>
+          <h2>{props.name}</h2>
+          <p>Description</p>
+          <div className="recipe">
+            <button className="recipe-button" onClick={toggleNutrientsVisibility}>
+              Recipe
+            </button>
           </div>
         </div>
+        {isNutrientsVisible && (
+          <div className="nutrients">
+            <div className="nutrientssub1">
+              <img src="protein" alt="Protein" />
+              <p>Calory</p>
+              <p>{props.calory}</p>
+            </div>
+            <div className="nutrientssub2">
+              <img src="protein" alt="Protein" />
+              <p>Protein</p>
+              <p>{props.protein}</p>
+            </div>
+            <div className="nutrientssub3">
+              <img src="protein" alt="Protein" />
+              <p>Fat</p>
+              <p>{props.fat}</p>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
